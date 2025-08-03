eac8e25b8f5ee1b87ac69900a1c20441c250dde5
XIII <p.rumyantsev@dppm.pro>
2025-08-03 16:55:23 +0300

    2-random-api

---
+++ 2-random-api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	router := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(getRandomNumber()))
+		return
+	})
+
+	server.ListenAndServe()
+}
+
+func getRandomNumber() []byte {
+	return []byte(strconv.Itoa(rand.Intn(7)))
+}
